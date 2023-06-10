bdfdea3aaf384253010ff872c7fd2614204e8d52
Emanoel <emanoelsilvaif@gmail.com>
2023-06-10 00:18:26 -0300

    adicionando o elemento P com um espaçamento de 20px entre eles

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,34 @@
 <template>
-    <div id="container">
-      <table>
-        <tr>
-          <th>Nome</th>
-          <th>Idade</th>
-        </tr>
-
-        <tr>
-          <td>João</td>
-          <td>25</td>
-        </tr>
-      </table>
-
-      <button>Play</button>
+  <div class="container">
+    <h1>Dentro dessa div há um espaçamento de 20px entre os elementos P</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti delectus ea expedita cum sequi accusantium, praesentium eum explicabo amet quos sit eveniet libero error provident porro dignissimos facilis hic quidem.</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex excepturi quas quo? Eligendi veniam accusamus perferendis! Repellendus voluptas deleniti ut explicabo quidem distinctio qui provident placeat nulla quia, sequi alias?</p>
   </div>
+
+
+  <table>
+    <tr>
+      <th>Nome</th>
+      <th>Idade</th>
+    </tr>
+
+    <tr>
+      <td>João</td>
+      <td>25</td>
+    </tr>
+  </table>
+
+  <div>
+    <button>Play</button>
+  </div>
+
+      
 </template>
 
 <script>
 
 export default {
-  name: 'App'
+  name: 'App',
 }
 </script>
 
@@ -33,6 +42,11 @@ export default {
   margin-top: 60px;
   align-items: center;
 }
+
+.container, p {
+  margin-top: 20px;
+}
+
 
 table, th, td {
   border: 1px solid black;
